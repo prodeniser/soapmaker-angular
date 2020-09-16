2fd8d5720bf3ccfb1c2325e202e317ba203cb5cd
Preston Rodeniser <prodeniser@gmail.com>
2020-09-16 12:29:19 -0400

    Recipe initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { InventoryItemComponent } from './inventory/inventory-item/inventory-item.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeComponent } from './recipe-book/recipe/recipe.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
+import { InventoryItemEditComponent } from './inventory/inventory-item-edit/inventory-item-edit.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InventoryComponent,
+    InventoryItemComponent,
+    RecipeBookComponent,
+    RecipeComponent,
+    RecipeItemComponent,
+    RecipeEditComponent,
+    RecipeDetailsComponent,
+    InventoryItemEditComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
